a4af886b7dd47d181fe64a7b94470daea7915e53
CHenriqueRibeiro <chmr66@gmail.com>
2025-04-12 20:22:35 -0300

    Criação da opção para entrar em contato com o suporte

--- src/Components/DashboardSidebar/index.jsx
+++ src/Components/DashboardSidebar/index.jsx
@@ -5,7 +5,15 @@ import { DashboardLayout } from "@toolpad/core/DashboardLayout";
 import { AppProvider } from "@toolpad/core/AppProvider";
 import AnalyticsIcon from "@mui/icons-material/Analytics";
 import StorefrontIcon from "@mui/icons-material/Storefront";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import { Navigate, Outlet } from "react-router-dom";
+import {
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Tooltip,
+} from "@mui/material";
 
 const NAVIGATION = [
   {
@@ -31,6 +39,48 @@ const demoTheme = createTheme({
     },
   },
 });
+function SidebarFooter() {
+  return (
+    <>
+      <Tooltip title="Suporte">
+        <a
+          href="https://wa.me/5585991673309"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <ListItem
+            button
+            sx={{
+              px: 2,
+              py: 2,
+            }}
+          >
+            <ListItemIcon sx={{ color: "#6A1B9A", minWidth: "auto" }}>
+              <WhatsAppIcon sx={{ fontSize: 28 }} />
+            </ListItemIcon>
+            <ListItemText
+              primaryTypographyProps={{
+                sx: { color: "#6A1B9A", fontWeight: 500 },
+              }}
+            />
+          </ListItem>
+        </a>
+      </Tooltip>
+    </>
+  );
+}
+
+function CustomAppTitle() {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      spacing={2}
+      color={"#6A1B9A"}
+    ></Stack>
+  );
+}
 
 export default function DashboardLayoutBasic(props) {
   // eslint-disable-next-line react/prop-types
@@ -41,6 +91,10 @@ export default function DashboardLayoutBasic(props) {
   return (
     <AppProvider navigation={NAVIGATION} theme={demoTheme} window={demoWindow}>
       <DashboardLayout
+        slots={{
+          appTitle: CustomAppTitle,
+          sidebarFooter: SidebarFooter,
+        }}
         sx={{
           "& .MuiAppBar-root": {
             height: "5rem",
@@ -53,9 +107,6 @@ export default function DashboardLayoutBasic(props) {
             pt: 2,
             borderColor: "#F1EEFF",
             background: "#F9F8FF",
-          },
-          "& .MuiStack-root.css-1d9cypr-MuiStack-root": {
-            display: "none",
           },
           "& .MuiListItemText-root.MuiTypography-root.MuiTypography-body1": {
             color: "#AC42F7",
